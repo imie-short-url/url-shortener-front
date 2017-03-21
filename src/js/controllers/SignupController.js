9862a39c4215b9c65b0d04d1a9009c58638a0b81
Gwendal Le Doussal <contact@gwendal-ledoussal.fr>
2017-03-21 16:20:08 +0100

    Alert box

--- src/js/controllers/SignupController.js
+++ src/js/controllers/SignupController.js
@@ -3,13 +3,20 @@ minifyApp.controller('SignupController', function($scope, $http) {
         email: '',
         password: '',
         passwordConfirm: ''
-    }
+    };
+
+    $scope.errors = {
+        ERR_EMAIL_INVALID: "L'adresse email n'est pas valide.",
+        ERR_PASSWORD_INVALID: "Le mot de passe n'est pas valide.",
+        ERR_EMAIL_EMPTY: "L'email est vide.",
+        ERR_PASSWORD_EMPTY: "Le mot de passe est vide.",
+        ERR_EMAIL_PASSWORD_EMPTY: "Email et mot de passe vides.",
+        ERR_EMAIL_TAKEN: "L'email est déjà prise."
+    };
+
+    $scope.error = "";
 
     $scope.sendForm = function() {
-        if($scope.signupForm.$valid) {
-            console.log('sent');
-        } else {
-            console.log('nope');
-        }
+        $scope.error = $scope.errors.ERR_EMAIL_TAKEN;
     };
 });
