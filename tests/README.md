96abb69208c8148655d848acdc6add595d83503c
Thomas MEREL <thomas.merel.44@gmail.com>
2017-03-21 16:36:14 +0100

    maj test plan

---
+++ tests/README.md
@@ -0,0 +1,15 @@
+#### Test plan email
+''                  :   Fail
+'this@this.com'     :   Pass
+'skdsdfsdfdsf'      :   Fail
+'kjnzef@kjznef'     :   Fail
+'lksdf.com'         :   Fail
+
+#### Test plan password
+''                  :   Fail
+'qsAjdn?765976'     :   Pass
+'azd'               :   Fail
+'azesdsdfdsf'       :   Fail
+'jsdf9870jd'        :   Fail
+'lksdjvn======'     :   Fail
+
--- tests/spec.js
+++ tests/spec.js
@@ -3,36 +3,71 @@ describe('Test URL shortener', function() {
 
     var title = 'Super Calculator';
 
+    //login test success
+    it('should have a title', function() {
+        
+        var url = 'file:///home/vulture/Documents/gitProject/url-shortener-front/this.html';
+        browser.get(url);
+        expect(browser.getTitle()).toEqual(title);
+
+        var username = element(by.model('user.email'));
+        var password = element(by.model('user.password'));
+        var submitBut = element(by.css('mdbutton[type=submit]'));
+
+        username.sendKeys('testUser@gmail.com');
+        password.sendKeys('password');
+
+        submitBut.click();
+
+    });
+    
+    //login test fail mail
+    it('should have a title', function() {
+        
+        var url = 'file:///home/vulture/Documents/gitProject/url-shortener-front/this.html';
+        browser.get(url);
+        expect(browser.getTitle()).toEqual(title);
+
+        var username = element(by.model('user.email'));
+        var password = element(by.model('user.password'));
+        var submitBut = element(by.css('mdbutton[type=submit]'));
+
+        username.sendKeys('testUser@gmam');
+        password.sendKeys('password');
+
+        submitBut.click();
+
+    });
+
+     //login test fail mdp
+    it('should have a title', function() {
+        
+        var url = 'file:///home/vulture/Documents/gitProject/url-shortener-front/this.html';
+        browser.get(url);
+        expect(browser.getTitle()).toEqual(title);
+
+        var username = element(by.model('user.email'));
+        var password = element(by.model('user.password'));
+        var submitBut = element(by.css('mdbutton[type=submit]'));
+
+        username.sendKeys('testUser@gmail');
+        password.sendKeys('');
+
+        submitBut.click();
+
+    });
+
     //login test
     it('should have a title', function() {
-        browser.get('[URL]');
+        
+        var url = 'file:///home/vulture/Documents/gitProject/url-shortener-front/this.html';
+        browser.get(url);
         expect(browser.getTitle()).toEqual(title);
-
-        var username = element(by.model('user.name'));
-        var password = element(by.model('user.pass'));
-        var submitBut = element(by.model('submit'));
-
-        username.sendKeys('testUser');
-        password.sendKeys('password');
+        
+        var submitBut = element(by.css('mdbutton[type=submit]'));
 
         submitBut.click();
 
     });
 
-    //Register test
-    it('should have a title', function(){
-
-        browser.get('[URL]');
-        expect(browser.getTitle()).toEqual(title);
-
-        var username = element(by.model('user.name'));
-        var password = element(by.model('user.pass'));
-        var submitBut = element(by.model('submit'));
-
-        username.sendKeys('testUser');
-        password.sendKeys('password');
-
-        submitBut.click();
-    
-    });
 });
