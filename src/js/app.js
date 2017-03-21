b815d3470039fd11c0c80ab91bbe260a9c80833c
Gwendal Le Doussal <contact@gwendal-ledoussal.fr>
2017-03-21 13:34:08 +0100

    Signup form

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-const minifyApp = angular.module('MinifyApp', ['ngMaterial', 'ui.router']);
+const minifyApp = angular.module('MinifyApp', ['ngMaterial', 'ui.router', 'ngMessages']);
 
 minifyApp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/signin');
@@ -18,4 +18,30 @@ minifyApp.config(function($stateProvider, $urlRouterProvider) {
         templateUrl: 'views/dashboard.html',
         controller: "DashboardController as dashboardController"
     });
+});
+
+
+particlesJS.load('particles', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+minifyApp.directive('confirmPwd', function($interpolate, $parse) {
+  return {
+    require: 'ngModel',
+    link: function(scope, elem, attr, ngModelCtrl) {
+
+      var pwdToMatch = $parse(attr.confirmPwd);
+      var pwdFn = $interpolate(attr.confirmPwd)(scope);
+
+      scope.$watch(pwdFn, function(newVal) {
+          ngModelCtrl.$setValidity('password', ngModelCtrl.$viewValue == newVal);
+      })
+
+      ngModelCtrl.$validators.password = function(modelValue, viewValue) {
+        var value = modelValue || viewValue;
+        return value == pwdToMatch(scope);
+      };
+
+    }
+  }
 });
--- src/js/controllers/SignupController.js
+++ src/js/controllers/SignupController.js
@@ -1,7 +1,15 @@
-minifyApp.controller('SignupController', function($scope) {
+minifyApp.controller('SignupController', function($scope, $http) {
     $scope.user = {
         email: '',
         password: '',
         passwordConfirm: ''
     }
+
+    $scope.sendForm = function() {
+    	if($scope.signupForm.$valid) {
+    		console.log('sent');
+    	} else {
+    		console.log('nope');
+    	}
+    };
 });
